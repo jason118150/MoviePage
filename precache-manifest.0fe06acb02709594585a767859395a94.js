63c8bf0c5d506d12eb922abaa53c5ca5bb3ee981
jason118150 <jason18153@gmail.com>
2020-06-20 19:11:29 +0800

    Updates

---
+++ precache-manifest.0fe06acb02709594585a767859395a94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e7e529e05e311a69e059ddbd3910d3",
+    "url": "/moviepage/index.html"
+  },
+  {
+    "revision": "b7043829b34f14931b63",
+    "url": "/moviepage/static/js/2.69263915.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moviepage/static/js/2.69263915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa20c1729ab8f4b1f108",
+    "url": "/moviepage/static/js/main.b5472c37.chunk.js"
+  },
+  {
+    "revision": "66248cfbf9bf537757b6",
+    "url": "/moviepage/static/js/runtime-main.6fcd4c06.js"
+  }
+]);
